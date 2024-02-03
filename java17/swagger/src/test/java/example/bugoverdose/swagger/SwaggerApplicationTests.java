05885a2f2d7bd2016cdd7b38446bbf2fd02e804e
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-03 13:34:08 +0900

    Setup java17/swagger

---
+++ java17/swagger/src/test/java/example/bugoverdose/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,12 @@
+package example.bugoverdose.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
