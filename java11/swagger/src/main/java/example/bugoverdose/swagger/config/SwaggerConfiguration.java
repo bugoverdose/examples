dd0356c580ae0293d15a8a63d3e5d6d7708a4c62
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-03 18:58:01 +0900

    feat(java11/swagger): filter controllers

--- java11/swagger/src/main/java/example/bugoverdose/swagger/config/SwaggerConfiguration.java
+++ java11/swagger/src/main/java/example/bugoverdose/swagger/config/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package example.bugoverdose.swagger.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,7 +18,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(getApiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
