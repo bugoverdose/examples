bf46cf6edbb86069b750a7e2152fbce5b6e6c7ff
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-03 18:15:37 +0900

    feat(java11/swagger): add Docket

---
+++ java11/swagger/src/main/java/example/bugoverdose/swagger/config/SwaggerConfiguration.java
@@ -0,0 +1,32 @@
+package example.bugoverdose.swagger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("스웨거 입문 프로젝트")
+                .version("beta")
+                .description("Java 11, Spring Boot 2 환경에서 스웨거 셋업하기")
+                .build();
+    }
+}
