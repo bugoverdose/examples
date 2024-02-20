d094675b09cc56b737aae90c6ab25d8f2d58dc06
Jeong Jinwoo <jwjeong96@gmail.com>
2024-02-20 23:33:00 +0900

    Setup java11/swagger_auth

---
+++ java11/swagger_auth/src/main/java/example/bugoverdose/swaggerauth/SwaggerAuthApplication.java
@@ -0,0 +1,12 @@
+package example.bugoverdose.swaggerauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerAuthApplication.class, args);
+    }
+}
